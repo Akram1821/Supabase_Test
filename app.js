86742d605c6cb25224be62dfe838ed4bc63ca9b8
Akram1821 <126412322+Akram1821@users.noreply.github.com>
2024-06-23 17:15:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Supabase configuration
+    const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
     const SUPABASE_URL = 'https://qgzgeanmtwuxiaeplbqg.supabase.co';
     const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnemdlYW5tdHd1eGlhZXBsYnFnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTExMDM2NiwiZXhwIjoyMDM0Njg2MzY2fQ.gV8GTwMcOfio_gnTH1RvvT4_Re5pimUk_kiAzLqAf-Q';
-    const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
     // Form submission handler
     document.getElementById('order-form').addEventListener('submit', async (event) => {
